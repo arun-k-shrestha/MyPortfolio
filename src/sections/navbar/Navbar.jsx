2353b5926937c554a1e6db75eb8132bd6b79da58
arun-k-shrestha <kajiarun3@gmail.com>
2022-11-28 14:33:31 -0800

    new additions

--- src/sections/navbar/Navbar.jsx
+++ src/sections/navbar/Navbar.jsx
@@ -1,7 +1,27 @@
+import dummy from "../../images/dummy.png"
+import data from "./data"
+import {VscColorMode} from "react-icons/vsc"
 import "./Navbar.css"
 
 function Navbar(){
-    return(<section id = "navbar"><h1>Navbar</h1></section>)
+
+    return (
+    <nav>
+        <div className="container nav__container">
+            <a href="index.html" className="nav_logo">
+                <img src={dummy} alt="dummyImg"/>
+            </a>
+            <ul className="nav__menu">
+                {data.map(props => <li  key={props.item}>
+                                        <a href={props.link}> {props.title}</a>
+                                    </li>            
+                        )
+                }
+            </ul>
+            <button id ="theme__icon"> <VscColorMode/> </button>
+        </div>
+    </nav>
+    )
 }
 
 export default Navbar
