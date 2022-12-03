36f48495fa84292ec133170098523792cf143278
arun-k-shrestha <kajiarun3@gmail.com>
2022-12-03 14:17:44 -0800

    changes

---
+++ src/sections/header/IconData.js
@@ -0,0 +1,9 @@
+import {BsGithub} from "react-icons/bs"
+import {AiFillLinkedin} from "react-icons/ai"
+
+const data = [
+    {id:1, link:"https://github.com/arun-k-shrestha", icon:<BsGithub/>},
+    {id:2, link:"https://www.linkedin.com/in/arunkaji6/", icon:<AiFillLinkedin/>}
+]
+
+export default data
